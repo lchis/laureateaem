8c3afebe731be36b276a09e4920ddf1f3e4787f9
Liviu Chis <liviu.chis@gmail.com>
2017-01-10 10:43:13 -0800

    Basic bundle setup

---
+++ bundle/src/main/java/com/mirum/laureate/KeyService.java
@@ -0,0 +1,6 @@
+package com.mirum.laureate;
+ 
+public interface KeyService {
+    public void setKey(int val); 
+    public String getKey();
+}
--- bundle/src/main/java/laureate-aem/package-info.java
+++
@@ -1,4 +0,0 @@
-@Version("1.0.0")
-package laureate-aem;
-
-import aQute.bnd.annotation.Version;
